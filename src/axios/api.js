c0479ed00fe3c1f4697373bd0768aa2da12e9eb8
Leonel Aguirre <leonel.aguirre182@gmail.com>
2023-11-14 18:25:50 -0600

    :sparkles: Environment variables implementation.

--- src/axios/api.js
+++ src/axios/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create()
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL
+const baseURL = process.env.EXPO_PUBLIC_API_BASE_URL
 
 const sendRequest = (
   method,
