ee61e1f3780a60de3c5555d407d38f327aed1831
Leonel Aguirre <leonel.aguirre182@gmail.com>
2023-11-16 10:46:48 -0600

    :sparkles: Font definitions added.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useCallback } from "react"
-import { View, Text, Button } from "react-native"
+import { View, Text } from "react-native"
+import * as SplashScreen from "expo-splash-screen"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import Login from "./src/screens/Login/Login"
 import { useFonts } from "expo-font"
-import * as SplashScreen from "expo-splash-screen"
+
+import Login from "./src/screens/Login/Login"
 
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
@@ -37,8 +38,24 @@ function RootScreen() {
 
 const App = () => {
   const [fontsLoaded, fontError] = useFonts({
+    "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-Thin-Italic": require("./assets/fonts/Poppins-ThinItalic.ttf"),
+    "Poppins-ExtraLight": require("./assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-ExtraLight-Italic": require("./assets/fonts/Poppins-ExtraLightItalic.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Light-Italic": require("./assets/fonts/Poppins-LightItalic.ttf"),
     Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
-    // Poppins: require("./assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Italic": require("./assets/fonts/Poppins-Italic.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Medium-Italic": require("./assets/fonts/Poppins-MediumItalic.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-SemiBold-Italic": require("./assets/fonts/Poppins-SemiBoldItalic.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Bold-Italic": require("./assets/fonts/Poppins-BoldItalic.ttf"),
+    "Poppins-ExtraBold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraBold-Italic": require("./assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+    "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Black-Italic": require("./assets/fonts/Poppins-BlackItalic.ttf"),
   })
 
   const onLayoutRootView = useCallback(async () => {
