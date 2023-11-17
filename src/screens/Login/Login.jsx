2e74727204b0f24bb5a75e40ab6f4740c26c62cb
Leonel Aguirre <leonel.aguirre182@gmail.com>
2023-11-16 21:14:36 -0600

    :sparkles: Token stored through async storage.

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,86 +1,59 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import * as WebBrowser from "expo-web-browser"
 import { View, StyleSheet, Text } from "react-native"
 import { Image } from "expo-image"
 import { makeRedirectUri, useAuthRequest } from "expo-auth-session"
 import { LinearGradient } from "expo-linear-gradient"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
-import { get, post } from "../../axios/api"
+import { post } from "../../axios/api"
 
 import { Color, Space } from "styles"
 import { Button } from "components"
 
 WebBrowser.maybeCompleteAuthSession()
 
-// Endpoint
-const discovery = {
+const authEndpoints = {
   authorizationEndpoint: "https://accounts.spotify.com/authorize",
   tokenEndpoint: "https://accounts.spotify.com/api/token",
 }
 
 const Login = ({ navigation }) => {
-  const [request, response, promptAsync] = useAuthRequest(
+  const [_request, response, promptAsync] = useAuthRequest(
     {
       clientId: "28336657c5194faaa73c9a853e21b536",
       scopes: ["user-read-private", "user-read-email", "user-top-read"],
-      // To follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
-      // this must be set to false
       usePKCE: false,
       redirectUri: makeRedirectUri({ native: "exp://192.168.100.3:8081" }),
-      // clientSecret: "59777af05842484ab682e0436909159c",
-      // extraParams: "?show_dialog=true",
-      // show_dialog: true,
     },
-    discovery
+    authEndpoints
   )
 
-  React.useEffect(() => {
-    if (response?.type === "success") {
-      const { code } = response.params
-
-      // console.log({ params: response.params })
-
-      // const tokenResponse = TokenResponse.fromQueryParams(response.params)
-
-      // console.log({ tokenResponse })
-
-      post("/login", {
-        code: code,
-        redirectURL: makeRedirectUri({ native: "exp://192.168.100.3:8081" }),
-      })
-        // post("/user-data", {
-        //   token: code,
-        // })
-        // get("/spotify-auth-url", {
-        //   // token: code,
-        //   redirectURL: "exp://192.168.100.3:8081",
-        // })
-        .then((response) => {
-          // console.log("access_token", response.data)
-
-          post("/user-data", {
-            token: response.data.token,
-          })
-            .then((response) => {
-              console.log("response", response.data)
-
-              navigation.reset({
-                index: 0,
-                routes: [{ name: "Root" }],
-              })
-            })
-            .catch((error) => {
-              console.log("error", error.response.data)
-              // console.log("error", JSON.stringify(error, null, 2))
-            })
-
-          // if (response?.data?.url)
-          //   WebBrowser.openBrowserAsync(response?.data?.url)
+  useEffect(() => {
+    const login = async (code) => {
+      try {
+        const response = await post("/login", {
+          code: code,
+          redirectURL: makeRedirectUri({ native: "exp://192.168.100.3:8081" }),
         })
-        .catch((error) => {
-          // console.log("error", error.response.data)
-          // console.log("error", JSON.stringify(error, null, 2))
+
+        const {
+          data: { token },
+        } = response
+
+        await AsyncStorage.setItem("token", token)
+
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "Root" }],
         })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    if (response?.type === "success" && response?.params?.code) {
+      login(response?.params?.code)
     }
   }, [response])
 
